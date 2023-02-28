6d2671f5a52f40845e82ed42bd6776a7eba924b9
Fmajid29 <faizanmajid11@gmail.com>
2023-03-01 03:59:09 +0500

    created the add product page and routed every page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import "./App.css";
-import Product from "./Components/Product/Product";
 import AdminPanel from "./Pages/AdminPanel/AdminPanel";
 import Login from "./Pages/Login/Login";
+import AddProduct from "./Pages/AddProduct/AddProduct";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      {/* <Product /> */}
-      <AdminPanel />
-      {/* <Login /> */}
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Login />} />
+          <Route exact path="/adminpanel" element={<AdminPanel />} />
+          <Route exact path="/addproduct" element={<AddProduct />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/Pages/AddProduct/AddProduct.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../../Components/NavBar/Navrbar";
+import "./Addproduct.css";
+
+const AddProduct = () => {
+  return (
+    <div className="Addproduct">
+      <Navbar />
+    </div>
+  );
+};
+
+export default AddProduct;
