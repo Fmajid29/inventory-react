64a447eba19e1ffc786e01da399728b042e62414
Fmajid29 <faizanmajid11@gmail.com>
2023-03-01 03:38:06 +0500

    completed the product card

--- src/Components/Product/Product.jsx
+++ src/Components/Product/Product.jsx
@@ -1,14 +1,20 @@
-import React from 'react'
-import img from './img.jpg';
-import './Product.css';
-const Product = () => {
-    return (
-        <div className='mainDiv'>
+import React from "react";
+import "./Product.css";
+import { Button } from "react-bootstrap";
 
+const Product = (props) => {
+  return (
+    <div className="mainDiv">
+      <h1>{props.name}</h1>
+      <h3>Rs {props.price}</h3>
+      <h3>{props.qty} pcs</h3>
+      <div className="quantity">
+        <Button id="minusbutton">-</Button>
+        <input type="text" placeholder="enter quatity" />
+        <Button id="plusbutton">+</Button>
+      </div>
+    </div>
+  );
+};
 
-
-        </div>
-    )
-}
-
-export default Product
+export default Product;
--- src/Pages/AdminPanel/AdminPanel.jsx
+++ src/Pages/AdminPanel/AdminPanel.jsx
@@ -1,13 +1,24 @@
 import React from "react";
 import "./AdminPanel.css";
 import Navbar from "../../Components/NavBar/Navrbar";
+import Product from "../../Components/Product/Product";
 
 const AdminPanel = () => {
   return (
     <div className="AdminPanel">
       <Navbar />
       <div className="container">
-        <div className="products"></div>
+        <div className="products">
+          <Product name="Name" price="400" qty="29" />
+          <Product name="Name" price="400" qty="29" />
+          <Product name="Name" price="400" qty="29" />
+          <Product name="Name" price="400" qty="29" />
+          <Product name="Name" price="400" qty="29" />
+          <Product name="Name" price="400" qty="29" />
+          <Product name="Name" price="400" qty="29" />
+          <Product name="Name" price="400" qty="29" />
+          <Product name="Name" price="400" qty="29" />
+        </div>
       </div>
     </div>
   );
