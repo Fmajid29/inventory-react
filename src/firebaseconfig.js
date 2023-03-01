da9800002db8fa6bfbe03fd389802998b3593aaf
Fmajid29 <faizanmajid11@gmail.com>
2023-03-01 12:48:34 +0500

    linked thr project to firebase

---
+++ src/firebaseconfig.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCI2tQyogO3cTlvp-hXfhUiU7bt-ZQIjts",
+  authDomain: "trew-a9dbc.firebaseapp.com",
+  projectId: "trew-a9dbc",
+  storageBucket: "trew-a9dbc.appspot.com",
+  messagingSenderId: "845577995478",
+  appId: "1:845577995478:web:637a262d8fc7635c9a3577",
+};
+
+export const app = initializeApp(firebaseConfig);
